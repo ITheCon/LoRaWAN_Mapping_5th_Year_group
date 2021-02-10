30f79d93537735d53eba6a9bf3f829d8b3d5d170
ITheCon <cksmith@net-ex.no>
2021-02-10 16:40:38 +0100

    Added leaflet offline repository
    https://github.com/allartk/leaflet.offline

---
+++ leaflet.offline-master/src/index.js
@@ -0,0 +1,23 @@
+import './TileLayerOffline';
+import './ControlSaveTiles';
+import {
+  getStorageInfo,
+  getStorageLength,
+  getTileUrls,
+  getStoredTilesAsJson,
+  removeTile,
+  truncate,
+  downloadTile,
+  saveTile,
+} from './TileManager';
+
+export {
+  downloadTile,
+  getStorageInfo,
+  getTileUrls,
+  getStorageLength,
+  truncate,
+  getStoredTilesAsJson,
+  removeTile,
+  saveTile,
+};
